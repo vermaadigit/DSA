7c9b0b4ec60142a4f8344c7948bd759416059265
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-04-21 23:28:00 +0530

    Commit

--- Array_Intro.java
+++ Array_Intro.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Array_Intro {
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] number = {2, 4, 6, 8, 10, 12, 14};
+
 
     }
 }
