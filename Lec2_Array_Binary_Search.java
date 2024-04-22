5a172653e83499c95dbe30cca67e26ad4c4d7d3e
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-04-22 23:34:53 +0530

    Commit

---
+++ Lec2_Array_Binary_Search.java
@@ -0,0 +1,33 @@
+public class Lec2_Array_Binary_Search {
+
+    public static int binarySearch(int[] numbers, int key)
+    {
+        int start = 0, end = numbers.length-1;
+
+        while (start <= end)
+        {
+            int mid = (start+end)/2;
+
+            if (numbers[mid] == key)
+            {
+                return mid;
+            }
+            if (numbers[mid] < key)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int[] number = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+
+        System.out.println("Index for key is " + binarySearch(number, key));
+    }
+}
