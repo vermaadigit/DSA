db63c1045f51267e0e290a533e7e0ecddf630583
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-04-22 12:55:47 +0530

    Commit

--- Array_Intro.java
+++ Array_Intro.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 
 public class Array_Intro {
+    public static int linearSearch(int[] number, int key){
+        for (int i = 0; i<number.length; i++)
+        {
+            if (number[i] == key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-
         int[] number = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
 
+        System.out.println("Index is " + linearSearch(number, key));
 
     }
 }
