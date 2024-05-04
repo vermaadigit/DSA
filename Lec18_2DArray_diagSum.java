7ca91dfcbb1189c4e5d30262792320761072c7c0
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-05-05 01:16:52 +0530

    Diagonal Sum 2D Array

--- Lec18_2DArray_diagSum.java
+++ Lec18_2DArray_diagSum.java
@@ -2,17 +2,28 @@ public class Lec18_2DArray_diagSum {
     public static int diagonalSum(int[][] matrix){
         int sum = 0;
 
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[0].length; j++){
-                if (i == j){
-                    sum += matrix[i][j];
-                }
-                else if (i+j == matrix.length-1){
-                    sum += matrix[i][j];
-                }
+//        for (int i = 0; i < matrix.length; i++){
+//            for (int j = 0; j < matrix[0].length; j++){
+//                if (i == j){
+//                    sum += matrix[i][j];
+//                }
+//                else if (i+j == matrix.length-1){
+//                    sum += matrix[i][j];
+//                }
+//
+//            }
+//        }
 
+        for (int i = 0; i < matrix.length; i++){
+            //Primary Diagonal
+            sum += matrix[i][i];
+
+            //Secondary Diagonal
+            if (i != matrix.length-1-i){
+                sum += matrix[i][matrix.length-1-i];
             }
         }
+
         return sum;
     }
     public static void main(String[] args){
