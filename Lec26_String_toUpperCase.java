fa81a912c629f2d5d2ff7e9a56486d7be1e4531e
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-05-06 00:15:16 +0530

    Commit

--- Lec26_String_toUpperCase.java
+++ Lec26_String_toUpperCase.java
@@ -1,6 +1,25 @@
 public class Lec26_String_toUpperCase {
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++){
+            if (str.charAt(i) == ' ' && i < str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args){
 
         String str = "hi, i am aditya";
+
+        System.out.println(toUpperCase(str));
     }
 }
