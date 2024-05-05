ce9c9e94c78db0f934a8bebc14faac860915634e
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-05-05 14:24:14 +0530

    Basic of String

--- Lec21_String.java
+++ Lec21_String.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class Lec21_String {
     public static void main(String[] args){
 
-        char[] arr = {'a','b','c','d'};
-        String str = "abcd";
-        String str2 = new String("xyz");
+//        char[] arr = {'a','b','c','d'};
+//        String str = "abcd";
+//        String str2 = new String("xyz");
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a String : ");
+//        String name;
+//        name = sc.nextLine();
+//        System.out.println("We Print - " + name);
+//
+//        String firstName = "Aditya";
+//        String lastName = " Verma";
+//
+//        String fullName = firstName + lastName;
+//        System.out.println(fullName);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String : ");
-        String name;
-        name = sc.nextLine();
-        System.out.println("We Print - " + name);
-
-        String firstName = "Aditya";
-        String lastName = " Verma";
-
-        String fullName = firstName + lastName;
-        System.out.println(fullName);
+        String name = "Aditya Verma";
+        System.out.println(name.charAt(2));
     }
 }
