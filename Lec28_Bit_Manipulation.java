3801ba589de0fa539888f081d28bb66474533a31
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-06-05 23:40:22 +0530

    Check Number is Even or Odd

--- Lec28_Bit_Manipulation.java
+++ Lec28_Bit_Manipulation.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Lec28_Bit_Manipulation {
+    public static boolean checkOddEven(int num){
+        int bitmask = 1;
+
+        if ((num & bitmask) == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number - ");
+        int num = sc.nextInt();
+
+        if (checkOddEven(num) == true){
+            System.out.print("Given Number is Even");
+        }
+        else {
+            System.out.println("Given Number is Odd");
+        }
     }
 }
