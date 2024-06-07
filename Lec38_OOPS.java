f54a90eb1f350b1cc522d0bd6a06371c42c272ca
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-06-07 23:29:05 +0530

    OOPS

--- Lec38_OOPS.java
+++ Lec38_OOPS.java
@@ -1,6 +1,29 @@
 public class Lec38_OOPS {
+    public static void main(String[] args){
+
+    }
+
 }
 
 class Pen {
+    String color;
+    int tip;
 
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calculatePercentage(int phy, int chem, int math){
+        percentage =  (float) (phy + chem + math) / 3;
+    }
 }
