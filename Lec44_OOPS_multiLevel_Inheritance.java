95fab7e3bdf8c9d565c473cb893b42b967d2e64b
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-06-10 15:41:14 +0530

    OOPS Multi-Level Inheritance

---
+++ Lec44_OOPS_multiLevel_Inheritance.java
@@ -0,0 +1,30 @@
+public class Lec44_OOPS_multiLevel_Inheritance {
+    public static void main(String[] args){
+        Dog dobby = new Dog();
+        dobby.eat();
+        dobby.breathe();
+        dobby.legs = 4;
+        System.out.println(dobby.legs);
+        dobby.breed = "German Sephardi";
+        System.out.println(dobby.breed);
+    }
+}
+
+class Animal1 {
+    String color;
+
+    void eat() {
+        System.out.println("Eats");
+    }
+    void breathe() {
+        System.out.println("Breathes");
+    }
+}
+
+class Mammal extends Animal1 {
+    int legs;
+}
+
+class Dog extends Mammal {
+    String breed;
+}
