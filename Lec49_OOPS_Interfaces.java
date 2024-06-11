4f730ddd0f20e26ec03f5ec506daa302ed3f1262
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-06-11 17:51:22 +0530

    OOPS Interface

---
+++ Lec49_OOPS_Interfaces.java
@@ -0,0 +1,30 @@
+public class Lec49_OOPS_Interfaces {
+    public static void main(String[] args){
+
+        Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer {
+
+    void moves();
+}
+
+class Queen implements ChessPlayer {
+    public void moves() {
+        System.out.println("Up, Down, Left, Right, Diagonal (in all 4 Direction)");
+    }
+}
+
+class Rook implements ChessPlayer {
+    public void moves() {
+        System.out.println("Up, Down, Left, Right");
+    }
+}
+
+class King implements ChessPlayer {
+    public void moves() {
+        System.out.println("Up, Down, Left, Right, Diagonal - (by 1 Step)");
+    }
+}
