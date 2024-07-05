9fc23bacb5c4358bac0c90f40c88a7d4d525aa56
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-05 11:38:14 +0530

    First Occurence

--- Lec57_Recursion_First_Occurence.java
+++ Lec57_Recursion_First_Occurence.java
@@ -1,5 +1,21 @@
-public class Lec57_Recursion_First_Occurence {
-    public static void main(String[] args){
+public class Lec57_Recursion_First_Occurence
+{
+    public static int firstOccurence(int[] arr, int key, int i)
+    {
+        if (i == arr.length)
+        {
+            return -1;
+        }
+        if (arr[i] == key)
+        {
+            return i;
+        }
 
+        return firstOccurence(arr, key, i+1);
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        System.out.println(firstOccurence(arr, 5, 0));
     }
 }
