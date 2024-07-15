3811c324bcd80182956c4310119f2b5c58ed3267
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-15 23:01:39 +0530

    BackTracking Array

---
+++ Lec70_BackTracking_Array.java
@@ -0,0 +1,30 @@
+public class Lec70_BackTracking_Array
+{
+    public static void changeArr(int[] arr, int i, int value)
+    {
+        //Base case
+        if (i == arr.length)
+        {
+            printArr(arr);
+            return;
+        }
+        //Recursion
+        arr[i] = value;
+        changeArr(arr, i+1, value+1);
+        arr[i] = arr[i] - 2;
+    }
+    public static void printArr(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
