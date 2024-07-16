9147c5cbc66efdb0359cbe522657e3e9bf11486c
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-16 15:11:19 +0530

    BackTracking N Queens

---
+++ Lec73_BackTracking_NQueens.java
@@ -0,0 +1,76 @@
+public class Lec73_BackTracking_NQueens
+{
+    public static boolean isSafe(char[][] board, int row, int col)
+    {
+        //Vertical Up
+        for (int i = row-1; i >= 0; i--)
+        {
+            if (board[i][col] == 'Q')
+            {
+                return false;
+            }
+        }
+        //Diagonal Left Up
+        for (int i = row - 1, j = col - 1; i >=0 && j >= 0; i--,j--)
+        {
+            if (board[i][j] == 'Q')
+            {
+                return false;
+            }
+        }
+        //Diagonal Right Up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--,j++)
+        {
+            if (board[i][j] == 'Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueens(char[][] board, int row)
+    {
+        //Base Case
+        if (row == board.length)
+        {
+            printBoard(board);
+            return;
+        }
+        for (int j = 0; j < board.length; j++)
+        {
+            if (isSafe(board, row, j))
+            {
+                board[row][j] = 'Q';
+                nQueens(board, row+1);      //Function Call
+                board[row][j] = 'x';            //BackTracking Step
+            }
+        }
+    }
+    public static void printBoard(char[][] board)
+    {
+        System.out.println("<---------- Chess Board ---------->");
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board.length; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        int n = 10;
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board.length; j++)
+            {
+                board[i][j] = 'x';
+            }
+        }
+
+        nQueens(board, 0);
+    }
+}
