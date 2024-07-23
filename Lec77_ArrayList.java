0b565df43d004414012f6566f820bdc7145d783f
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-23 18:33:43 +0530

    Array List

--- Lec77_ArrayList.java
+++ Lec77_ArrayList.java
@@ -7,5 +7,17 @@ public class Lec77_ArrayList
         //Java Collection Framework
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+
+        System.out.println(list.get(2));
+
+        System.out.println(list.contains(3));
     }
 }
