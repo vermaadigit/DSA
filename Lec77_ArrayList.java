335d65a3fed12981303cd046c3042ab2ec164ae5
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-23 11:44:46 +0530

    Array List Intro

--- Lec77_ArrayList.java
+++ Lec77_ArrayList.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Lec77_ArrayList
 {
     public static void main(String[] args)
     {
-
+        //Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
     }
 }
