9886ede31150457cb5aa3b4b7eb6b5118d421d83
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-25 02:20:32 +0530

    Array List Pair Sum 2 Pointer Approach

---
+++ Lec86_ArrayList_pairSum_2Pointer.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Lec86_ArrayList_pairSum_2Pointer
+{
+    //Two Pointer Concept Used
+    public static boolean pairValue(ArrayList<Integer> list, int target)
+    {
+        int lp = 0, rp = list.size() - 1;
+        while (lp != rp)
+        {
+            if (list.get(lp) + list.get(rp) == target)
+            {
+                return true;
+            }
+            else if (list.get(lp) + list.get(rp) < target)
+            {
+                lp++;
+            }
+            else
+            {
+                rp--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int target = 8;
+
+        System.out.println(pairValue(list, target));
+    }
+}
