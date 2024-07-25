624da56a4c0bde803aaea15569a0450d878641f0
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-25 20:32:11 +0530

    Linked List

---
+++ Lec88_ArrayList.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class Lec88_ArrayList
+{
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args)
+    {
+        LinkedList ll = new LinkedList();
+    }
+}
