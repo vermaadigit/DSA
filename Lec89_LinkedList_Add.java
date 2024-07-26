9218753221697213c85e2c03d55ee5e4dc85b485
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-26 11:26:22 +0530

    Linked List Add First and Add Last & also Print

--- Lec89_LinkedList_Add.java
+++ Lec89_LinkedList_Add.java
@@ -1,44 +1,36 @@
-import java.util.LinkedList;
-
-public class Lec89_LinkedList_Add
-{
-    public static class Node
-    {
+public class Lec89_LinkedList_Add {
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data)
-        {
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data)
-    {
-        //Step 1 - Create new Node
+    public void addFirst(int data) {
+        // Step 1 - Create new Node
         Node newNode = new Node(data);
 
-        if (head == null)
-        {
+        if (head == null) {
             head = tail = newNode;
             return;
         }
 
-        //Step 2 - newNode next = head
-        newNode.next = head; //linking
+        // Step 2 - newNode next = head
+        newNode.next = head; // linking
 
-        //Step 3 - head = newNode
+        // Step 3 - head = newNode
         head = newNode;
     }
 
-    public void addLast(int data)
-    {
+    public void addLast(int data) {
         Node newNode = new Node(data);
-        if (head == null)
-        {
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -46,13 +38,30 @@ public class Lec89_LinkedList_Add
         tail = newNode;
     }
 
-    public static void main(String[] args)
-    {
-        LinkedList ll = new LinkedList();
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        Lec89_LinkedList_Add ll = new Lec89_LinkedList_Add();
+
+        ll.print();
         ll.addFirst(2);
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addLast(3);
+        ll.print();
         ll.addLast(4);
+        ll.print();
     }
 }
