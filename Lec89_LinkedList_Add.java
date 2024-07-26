afa8bbee00908c1a835a30b32508639e33da081e
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-26 12:05:58 +0530

    Linked Add as in Middle

--- Lec89_LinkedList_Add.java
+++ Lec89_LinkedList_Add.java
@@ -45,23 +45,37 @@ public class Lec89_LinkedList_Add {
         }
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.data + " ");
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
-        System.out.println();
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data)
+    {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1)
+        {
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx - 1   temp --> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static void main(String[] args) {
         Lec89_LinkedList_Add ll = new Lec89_LinkedList_Add();
 
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.add(2, 9);
         ll.print();
     }
 }
